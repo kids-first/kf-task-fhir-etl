79d599a5e44389a69fb03d04c2308749ac90e23c
Natasha Singh <dukedesi22@gmail.com>
2023-02-28 15:45:11 -0500

    :technologist: Scripts to bootstrap development env

---
+++ bin/setup_dev_env.sh
@@ -0,0 +1,61 @@
+
+#!/bin/bash
+
+# Setup python virtual env with project dependencies
+# Setup docker compose stack
+
+# --- CLI Options ---
+
+# --delete-volumes will delete any volumes in the docker-compose stack
+# This means any data in Data Service or FHIR service will be lost
+
+# --ingest will ingest the SD_ME0WME0W test study into Data Service 
+
+set -e
+
+DELETE_VOLUMES=0
+INGEST_STUDY=0
+
+while [ -n "$1" ]; do 
+	case "$1" in
+	--delete-volumes) DELETE_VOLUMES=1 ;; 
+	--ingest) INGEST_STUDY=1 ;; 
+	*) echo "Option $1 not recognized" ;; 
+	esac
+	shift
+done
+
+echo "➡️  Begin development environment setup ..."
+
+if [ ! -d venv ]; then
+echo "\n🐍 Setup Python virtual env and install deps ..."
+    virtualenv venv
+    source venv/bin/activate
+    pip install -e .
+else
+    source venv/bin/activate
+fi
+
+# Delete docker-compose volumes
+if [ $DELETE_VOLUMES -eq 1 ]; then
+    echo "\n🗑️ Remove old volumes ..."
+    rm -rf ./.data/fhir_postgres
+    rm -rf ./.data/hapi
+fi
+
+echo "\n🐳 Start docker compose stack ..."
+source .env
+docker compose down
+docker compose up -d 
+
+if [ $DELETE_VOLUMES -eq 1 ]; then
+    echo "\n 👷‍♀️ Waiting for FHIR service to deploy (may take a minute) ..."
+    docker-compose logs -f fhir | grep -cm1 "Started Application in"
+fi
+
+if [ $INGEST_STUDY -eq 1 ]; then
+    ./bin/setup_study.sh
+fi
+
+echo "✅ --- Development environment setup complete! ---"
+
---
+++ bin/setup_study.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+# Create SD_ME0WME0W test study in Data Service
+# Ingest SD_ME0WME0W test study into Data Service
+
+set -e
+
+echo "\n ✨ Creating test study and sequencing center in Data Service ..."
+
+curl -f -X POST \
+-H "Content-Type: application/json" \
+-d '{"kf_id": "SD_ME0WME0W", "external_id": "cat-study"}' \
+$KF_API_DATASERVICE_URL/studies | jq '.results.kf_id'
+
+curl -f  -X POST \
+-H "Content-Type: application/json" \
+-d '{"kf_id": "SC_A1JNZAZH", "name": "Baylor"}' \
+$KF_API_DATASERVICE_URL/sequencing-centers | jq '.results.kf_id'
+
+# echo "\n 🏭 Ingest test study data into Data Service ..."
+kidsfirst ingest data/SD_ME0WME0W
+
