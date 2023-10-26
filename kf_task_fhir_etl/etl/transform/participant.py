db91afae099c34ff6f5721a49e29ba4046ec81dd
Natasha Singh <dukedesi22@gmail.com>
2023-10-26 14:05:21 +0000

    :recycle: Mv transform participant into own module

---
+++ kf_task_fhir_etl/etl/transform/participant.py
@@ -0,0 +1,42 @@
+
+import logging
+
+from kf_lib_data_ingest.common.concept_schema import CONCEPT
+from kf_lib_data_ingest.common.pandas_utils import outer_merge
+
+logger = logging.getLogger(__name__)
+
+def build_df(dataservice_entity_dfs_dict, study_merged_df, studies):
+    logger.info(
+        f"🏭 Transforming participants ..."
+    )
+    participants = dataservice_entity_dfs_dict.get("participants")
+
+    if participants is not None:
+        columns = {
+            "family_id": CONCEPT.FAMILY.TARGET_SERVICE_ID,
+            "study_id": CONCEPT.STUDY.TARGET_SERVICE_ID,
+            "affected_status": CONCEPT.PARTICIPANT.IS_AFFECTED_UNDER_STUDY,
+            "diagnosis_category": CONCEPT.STUDY.CATEGORY,
+            "ethnicity": CONCEPT.PARTICIPANT.ETHNICITY,
+            "external_id": CONCEPT.PARTICIPANT.ID,
+            "gender": CONCEPT.PARTICIPANT.GENDER,
+            "is_proband": CONCEPT.PARTICIPANT.IS_PROBAND,
+            "kf_id": CONCEPT.PARTICIPANT.TARGET_SERVICE_ID,
+            "race": CONCEPT.PARTICIPANT.RACE,
+            "species": CONCEPT.PARTICIPANT.SPECIES,
+            "visible": CONCEPT.PARTICIPANT.VISIBLE,
+        }
+        participants = participants[list(columns.keys())]
+        participants = participants.rename(columns=columns)
+        participants = participants[
+            participants[CONCEPT.PARTICIPANT.VISIBLE] == True
+        ]
+        if not participants.empty:
+            study_merged_df = outer_merge(
+                study_merged_df if study_merged_df is not None else studies,
+                participants,
+                with_merge_detail_dfs=False,
+                on=CONCEPT.STUDY.TARGET_SERVICE_ID,
+            )
+    return study_merged_df
